692337d488af1076082854ffcb32d8a05822fca7
Pradyut Basuri <basuri.pradyut@gmail.com>
2020-02-17 23:50:33 +0530

    model_relationshio

--- app/User.php
+++ app/User.php
@@ -16,7 +16,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'user_name','email','email_verified_at',  'password','first_name','last_name','mob_no','roll_type',
+        'emp_department_type','emp_image','emp_facebook_link','emp_twitter_link','emp_linkedin_link','remember_token',
+        'created_at','updated_at'
     ];
 
     /**
@@ -36,4 +38,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function department_name()
+    {
+        return $this->hasOne('App\tbl_departments','emp_department_type','id');
+    }
 }
